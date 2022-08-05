dda6998e5c71c4efa2ae697de8089baaab7e6ea6
KNSN92 <97819935+KNSN92@users.noreply.github.com>
2022-08-05 22:35:25 +0900

    add new commond

---
+++ src/assemblylang/commands/CommandPOW.java
@@ -0,0 +1,35 @@
+package assemblylang.commands;
+
+import java.util.Map;
+
+import assemblylang.Engine;
+import assemblylang.ICommand;
+
+public class CommandPOW implements ICommand {
+
+	@Override
+	public int runCommand(int[] input, Engine engine, Map<String, ?> customValueMap, int argCount) {
+		return (int) Math.pow(input[0], input[1]);
+	}
+
+	@Override
+	public boolean isRunnable(int[] input, Engine engine, Map<String, ?> customValueMap, int argCount) {
+		return true;
+	}
+
+	@Override
+	public int[] getArgCounts() {
+		return new int[] {2};
+	}
+
+	@Override
+	public String getReturnRegName() {
+		return "OP";
+	}
+
+	@Override
+	public int[] getNoConversionLocations() {
+		return null;
+	}
+
+}
