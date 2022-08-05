a924ef34e48d9b6a8cc6ea0a0e72729786457268
KNSN92 <97819935+KNSN92@users.noreply.github.com>
2022-08-05 22:36:17 +0900

    change.

--- src/test/Tester.java
+++ src/test/Tester.java
@@ -4,17 +4,30 @@ import java.io.File;
 import java.io.IOException;
 
 import assemblylang.Engine;
+import assemblylang.util.StringQuotationUtil;
 
 public class Tester {
 
 	public static void main(String[] args) {
+		debugPrint();
+		//runTestScript();
+	}
+	
+	private static int[] runTestScript() {
 		Engine engine = new Engine(8);
+		int[] result = new int[0];
 		engine.addReg("OPR");
 		try {
-			engine.run(new File("src/test/testcode.txt"));
+			result = engine.run(new File("src/test/testcode.txt"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		return result;
+	}
+	
+	private static void debugPrint() {
+		System.out.println(StringQuotationUtil.isStringFormat("\"teNst\""));
 	}
 
 }
