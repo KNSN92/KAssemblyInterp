89176c777c261d9d9fa2c5af100ae98c448ba34b
KNSN92 <97819935+KNSN92@users.noreply.github.com>
2022-08-30 01:22:34 +0900

    初コミット。

---
+++ src/assemblylang/commands/CommandEXPORT.java
@@ -0,0 +1,37 @@
+package assemblylang.commands;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import assemblylang.Engine;
+import assemblylang.ICommand;
+
+public class CommandEXPORT implements ICommand {
+	
+	public int ExportInfos[] = new int[0];
+
+	@Override
+	public int runCommand(int[] input, Engine engine, int argCount) {
+		ExportInfos = ArrayUtils.addAll(ExportInfos, input);
+		return 0;
+	}
+
+	@Override
+	public boolean isRunnable(int[] input, Engine engine, int argCount) {
+		return true;
+	}
+
+	@Override
+	public int[] getArgCounts() {
+		return null;
+	}
+
+	@Override
+	public String getReturnRegName() {
+		return null;
+	}
+	
+	@Override
+	public void reset() {
+		ExportInfos = new int[0];
+	}
+}
