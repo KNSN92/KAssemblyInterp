fb04501e53ae1378233f3732ad0c24cded9f5670
KNSN92 <97819935+KNSN92@users.noreply.github.com>
2022-08-31 18:55:27 +0900

    引数の内容を変更。

--- src/assemblylang/util/CommandFunction.java
+++ src/assemblylang/util/CommandFunction.java
@@ -1,9 +1,7 @@
 package assemblylang.util;
 
-import java.util.Map;
-
 import assemblylang.Engine;
 
 public interface CommandFunction<T> {
-	public T run(T arg, Engine engine, Map<String,?> customValueMap, int argCount);
+	public T run(T arg, Engine engine, int argCount);
 }
