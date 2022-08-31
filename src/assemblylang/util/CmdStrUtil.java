62d7ca8ba52219272f1cadff11643842b472bafd
KNSN92 <97819935+KNSN92@users.noreply.github.com>
2022-08-31 18:55:04 +0900

    indexOfRegexメソッドとcontainsRegexを追加。

--- src/assemblylang/util/CmdStrUtil.java
+++ src/assemblylang/util/CmdStrUtil.java
@@ -6,8 +6,7 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.Validate;
 
 public final class CmdStrUtil {
-	
-	
+
 	/**
 	 * 配列に指定した要素が一つだけ含まれているか確認します。
 	 * @param list 
@@ -20,7 +19,7 @@ public final class CmdStrUtil {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 配列の中にある要素の数をカウントします。
 	 * @param list
@@ -34,7 +33,7 @@ public final class CmdStrUtil {
 		}
 		return valcount;
 	}
-	
+
 	/**
 	 * 指定したインデックスを"0"に変更します。
 	 * @param list
@@ -47,7 +46,7 @@ public final class CmdStrUtil {
 		clone[index] = "0";
 		return clone;
 	}
-	
+
 	/**
 	 * keyWordsで指定した文字をkeyWordsに格納されている要素のインデックスに置き換えます。<br>
 	 * <br>
@@ -64,14 +63,13 @@ public final class CmdStrUtil {
 		int count = 0;
 		for (String cloneval : clone) {
 			if (ArrayUtils.contains(keyWords, clone)) {
-				clone[count] = Integer.toString
-					(ArrayUtils.indexOf(keyWords, cloneval));
+				clone[count] = Integer.toString(ArrayUtils.indexOf(keyWords, cloneval));
 			}
 			count++;
 		}
 		return clone;
 	}
-	
+
 	/**
 	 * keyWordsで指定した文字をkeyWordsに入っている数値に置き換えます。
 	 * @param list
@@ -83,14 +81,13 @@ public final class CmdStrUtil {
 		int count = 0;
 		for (String cloneval : clone) {
 			if (keyWords.containsKey(cloneval)) {
-				clone[count] = Integer.toString
-					(keyWords.get(cloneval));
+				clone[count] = Integer.toString(keyWords.get(cloneval));
 			}
 			count++;
 		}
 		return clone;
 	}
-	
+
 	/**
 	 * keyWordsで指定した文字列のいずれかがlistに含まれているかを調べます。
 	 * @param list 調べる配列
@@ -98,12 +95,14 @@ public final class CmdStrUtil {
 	 * @return listにkeyWordsのいずれかが含まれているならtrue。でなければfalse。
 	 */
 	public static boolean containsAny(String[] list, String[] keyWords) {
-		for(String listVal:list) {
-			if(isSameAnyWord(listVal,keyWords)) {return true;}
+		for (String listVal : list) {
+			if (isSameAnyWord(listVal, keyWords)) {
+				return true;
+			}
 		}
 		return false;
 	}
-	
+
 	/**
 	 * keyWordsで指定した文字列のいずれかがstrに含まれているかを調べます。
 	 * @param str 調べる文字列
@@ -113,7 +112,7 @@ public final class CmdStrUtil {
 	public static boolean isSameAnyWord(String str, String[] keyWords) {
 		return ArrayUtils.contains(keyWords, str);
 	}
-	
+
 	/**
 	 * matchWordsで指定した構成になっているか調べます。
 	 * @param list 調べる配列
@@ -124,9 +123,9 @@ public final class CmdStrUtil {
 		Validate.notNull(list);
 		Validate.notNull(matchWords);
 		int index = 0;
-		for(String[] words:matchWords) {
-			for(String word:words) {
-				if(!list[index].matches(word)) {
+		for (String[] words : matchWords) {
+			for (String word : words) {
+				if (!list[index].matches(word)) {
 					return false;
 				}
 			}
@@ -134,7 +133,7 @@ public final class CmdStrUtil {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * 引数の配列の中身の文字列を全て大文字に変換します。
 	 * @param strs 大文字にする配列
@@ -142,9 +141,38 @@ public final class CmdStrUtil {
 	 */
 	public static String[] toAllUpperCase(String[] strs) {
 		String[] outs = strs;
-		for(int i = 0; i < strs.length; i++) {
+		for (int i = 0; i < strs.length; i++) {
 			outs[i] = outs[i].toUpperCase();
 		}
 		return outs;
+	}
+
+	/**
+	 * 引数の配列の中身の文字列から、正規表現にマッチする最初のインデックスを返します。
+	 * @param args
+	 * @param regex
+	 * @return index
+	 */
+	
+	public static int indexOfRegex(String[] args, String regex) {
+		Validate.noNullElements(args);
+		Validate.notNull(regex);
+		for(int i=0;i<args.length;i++) {
+			if(args[i].matches(regex)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * 引数の配列の中身の文字列に、正規表現にマッチする物があるか調べます。
+	 * @param args
+	 * @param regex
+	 * @return result
+	 */
+	
+	public static boolean containsRegex(String[] args, String regex) {
+		return CmdStrUtil.indexOfRegex(args, regex) != -1;
 	}
 }
